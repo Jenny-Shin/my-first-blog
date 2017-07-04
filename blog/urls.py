e9f96c8b51e3f464e43a4d0b5f96ed4e67e04ba9
ds4132 <ds4132@naver.com>
2017-07-04 10:48:06 +0900

    Added view and template for detailed blog post as wel as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     #http://127.0.0.1:8000/ 을 식별
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
